bb9657c8bb5407a6eabb8d4d26e971483ba8efd2
hckim96 <hyeoncheolkim96@gmail.com>
2022-09-22 00:12:45 +0900

    revert_filterScanRun

--- project1/submission/package.sh
+++ project1/submission/package.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # tar --dereference --exclude='build' --exclude='submission.tar.gz' --exclude='workloads' -czf submission.tar.gz *
-tar --dereference --exclude='build' --exclude='submission.tar.gz' --exclude='workloads' --exclude='excludeMyRunTestHarness.sh' --exclude='driverInput.txt' --exclude='myTest.sh' --exclude='myPackage.sh' -czf submission.tar.gz *
+tar --dereference --exclude='build' --exclude='submission.tar.gz' --exclude='workloads' --exclude='excludeMyRunTestHarness.sh' --exclude='driverInput.txt' --exclude='myTest.sh' --exclude='myPackage.sh' --exclude='compileDebug.sh' -czf submission.tar.gz *
